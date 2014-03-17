a1fcabfb06adea67dd54835521551022c71e6724
Vincent Saluzzo <vincent.saluzzo@itelios.com>
2014-03-17 22:11:20 +0100

    update

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.multipart());
+app.use(express.multipart({limit: '1024mb'}));
 app.use(express.methodOverride());
 app.use(express.cookieParser('azertyuio'));
 app.use(express.session());
