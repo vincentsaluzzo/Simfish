ba8fb03d359ba8cf96750fe977b55775a54d33df
Vincent Saluzzo <vincent.saluzzo@itelios.com>
2014-03-17 19:31:36 +0100

    remove unwanted file

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get('/', routes.index);
 app.post('/upload', routes.upload);
 app.get('/download/:id', routes.download);
 app.get('/view/:id', routes.view);
-app.get('/debug', routes.debug);
+//app.get('/debug', routes.debug);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
